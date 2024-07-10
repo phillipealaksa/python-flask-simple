860443563969f8e28226777062b11845999a2e9f
Phillipe Alaksa <phillipe.alaksa2407@gmail.com>
2024-07-10 11:30:38 +0200

    added Person + House classes

---
+++ main.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, dob, email, phone):
+        self.name = name
+        self.dob = dob
+        self.email = email
+        self.phone = phone
+
+class House:
+    def __init__(self, address, people):
+        self.address = address
+        self.people = people
